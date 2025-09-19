908ffae71c4bab418cba66817c22d6d376937449
malefax <shiven522002@gmail.com>
2025-09-19 05:49:41 +0530

    zombie mode

--- Adrishya.c
+++ Adrishya.c
@@ -221,13 +221,13 @@ static int __init mkdir_monitor_init(void)
     int err;
     size_t i;
     // not-stealthy as much but prevent module loading
- /* bool isalive;
+   /* bool isalive;
     if ( try_module_get(THIS_MODULE) ){
             
     }
     else {
             THIS_MODULE->state=MODULE_STATE_GOING;
-    } /*       
+    }*/        
 
 
     // Do kernel module hiding
